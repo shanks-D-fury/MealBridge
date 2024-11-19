cb5beb88a7232c6456346b6177080f7bd2c852f1
Shashank_S <shashanks9190@gmail.com>
2024-11-19 15:49:28 +0530

    BackEnd : Models Created for the database

---
+++ models/foodBank.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const foodBankSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	location: String,
+	country: String,
+	products: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "product",
+		},
+	],
+	geometry: {
+		type: {
+			type: String,
+			enum: ["Point"],
+			required: true,
+		},
+		coordinates: {
+			type: [Number],
+			required: true,
+		},
+	},
+});
+
+module.exports = mongoose.model("foodBank", foodBankSchema);
