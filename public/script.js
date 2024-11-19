b3193dea3e55e60dbe1e11763879c6d5ea1b38b7
Prathvihan1008 <prathvihan1008@gmail.com>
2024-11-19 22:33:41 +0530

    changed location of index.html

--- public/script.js
+++ public/script.js
@@ -6,10 +6,10 @@ function toggleWidget(widget) {
     // Check if content is collapsed or expanded
     if (content.style.height === '' || content.style.height === '0px') {
       content.style.height = content.scrollHeight + 'px';  // Expand
-      toggleimg.src = "./images/cross.svg";  // Change image to cross when expanded
+      toggleimg.src = "../public/images/cross.svg";  // Change image to cross when expanded
     } else {
       content.style.height = '0px';  // Collapse
-      toggleimg.src = "./images/plus.svg";  // Change image to plus when collapsed
+      toggleimg.src = "../public/images/plus.svg";  // Change image to plus when collapsed
     }
   }
   
