863e5adf6aa466b803758d50e5c8aa9929a2cb9f
Shashank_S <shashanks9190@gmail.com>
2024-11-23 23:15:18 +0530

    FrontEnd : Merging some new features from the main branch
     still pending

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const userSchema = new Schema({
 		required: true,
 	},
 	phoneNo: {
-		type: String,
+		type: Number,
 		required: true,
 	},
 });
--- public/JS/script.js
+++ public/JS/script.js
@@ -20,18 +20,3 @@
 		);
 	});
 })();
-
-function toggleWidget(widget) {
-	// Get the content and image inside the clicked widget
-	var content = widget.querySelector(".content");
-	var toggleimg = widget.querySelector(".toggleimg");
-
-	// Check if content is collapsed or expanded
-	if (content.style.height === "" || content.style.height === "0px") {
-		content.style.height = content.scrollHeight + "px"; // Expand
-		toggleimg.src = "/images/cross.svg"; // Change image to cross when expanded
-	} else {
-		content.style.height = "0px"; // Collapse
-		toggleimg.src = "/images/plus.svg"; // Change image to plus when collapsed
-	}
-}
